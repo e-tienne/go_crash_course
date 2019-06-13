fc5e687e00e1bf23bb300acb1c243248cc55329b
Etienne Simard <etienne@redhat.com>
2019-06-13 16:06:30 -0400

    Initial creation of array example

---
+++ 05_arrays_slices/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	// // Arrays - need to be a fixed lenght and typed
+	// var fruitArr [2]string
+
+	// // Assign values
+	// fruitArr[0] = "Apple"
+	// fruitArr[1] = "Orange"
+
+	// Declare and assign values in 1 step / 1 line instead of 2 steps / 3 lines
+	fruitArr := [2]string{"Apple", "Orange"}
+
+	fmt.Println(fruitArr)
+	fmt.Println(fruitArr[1])
+
+}
