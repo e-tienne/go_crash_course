bdc0eb302167cb09723a2d21544ff350ef212a72
Etienne Simard <etienne@redhat.com>
2019-06-13 16:11:10 -0400

    Added slice examples to 05_array_slices

--- 05_arrays_slices/main.go
+++ 05_arrays_slices/main.go
@@ -14,9 +14,17 @@ func main() {
 	// fruitArr[1] = "Orange"
 
 	// Declare and assign values in 1 step / 1 line instead of 2 steps / 3 lines
-	fruitArr := [2]string{"Apple", "Orange"}
+	// fruitArr := [2]string{"Apple", "Orange"}
 
-	fmt.Println(fruitArr)
-	fmt.Println(fruitArr[1])
+	// fmt.Println(fruitArr)
+	// fmt.Println(fruitArr[1])
+
+	// Slice example - not a fixed lenght
+
+	fruitSlice := []string{"Apple", "Orange", "Grape", "Cherry"}
+
+	fmt.Println(fruitSlice)
+	fmt.Println(len(fruitSlice))
+	fmt.Println(fruitSlice[1:3])
 
 }
