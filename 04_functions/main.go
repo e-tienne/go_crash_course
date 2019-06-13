59701d43f74e4ca24d59b96fea039cbc5aefaa35
Etienne Simard <etienne@redhat.com>
2019-06-13 15:56:17 -0400

    Added 04_functions

---
+++ 04_functions/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func greeting(name string) string {
+	return "hello " + name
+}
+
+func getSum(num1 int, num2 int) int {
+	return num1 + num2
+}
+func main() {
+
+	fmt.Println(greeting("etienne"))
+	fmt.Println(getSum(3, 4))
+}
